e47365f98bd98455fc1fc6ad22e1a5671f698ce0
Margaux RIANT <mriant@e2r6p7.clusters.42paris.fr>
2022-05-14 18:22:34 +0200

    print void grid and background

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ fn cstr(s: &str) -> &CStr {
     CStr::from_bytes_with_nul(s.as_bytes()).unwrap()
 }
 
-const WIDTH: u32 = 1280;
-const HEIGHT: u32 = 720;
+const WIDTH: u32 = 420;
+const HEIGHT: u32 = 494;
 
 fn main() {
     let mlx = Mlx::init().expect("Failed to initialize the MiniLibX.");
@@ -24,12 +24,8 @@ fn main() {
     let green_alpha = mlx.create_image_from_xpm_file( cstr("assets/alphabet_green.xpm\0")).unwrap();
     let yellow_alpha = mlx.create_image_from_xpm_file( cstr("assets/alphabet_yellow.xpm\0")).unwrap();
     let grey_alpha = mlx.create_image_from_xpm_file(cstr("assets/alphabet_grey.xpm\0")).unwrap();
-    draw_square(64, 640, 360, 4, &img);
+    init_bg(&img);
     win.put_image(&img, 0, 0);
-    win.put_image(&black_alpha, 0, 0);
-    win.put_image(&yellow_alpha, 0, 64);
-    win.put_image(&green_alpha, 0, 128);
-    win.put_image(&grey_alpha, 0, 192);
     mlx.hook_loop(move || {});
     let mlx3 = mlx.clone();
     win.hook(move |KeyPress(keycode)| {
