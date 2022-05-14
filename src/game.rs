5dd423c32f4106ba27e4ae40ec03b667c6071b01
nmathieu <nmathieu@student.42.fr>
2022-05-14 18:55:07 +0200

    basic wordle logic

--- src/game.rs
+++ src/game.rs
@@ -32,14 +32,92 @@ pub enum Letter {
 pub enum Correctness {
 	Correct,
 	Misplaced,
-	Wrong,
+	Incorrect,
 }
 
 pub struct Game {
-	pub winning_word: String,
+	pub winning_word: [Letter; Self::WORD_SIZE],
 
-	pub current_word: [Letter; 5],
+	pub current_word: [Letter; Self::WORD_SIZE],
 	pub cursor: usize,
 
-	pub previous_words: [[(Letter, Correctness); 5]; 6],
+	pub previous_words: [[(Letter, Correctness); Self::WORD_SIZE]; Self::MAX_TRIES],
+	pub current_try: usize,
+}
+
+impl Game {
+	pub const WORD_SIZE: usize = 5;
+	pub const MAX_TRIES: usize = 6;
+
+	pub fn new(winning_word: [Letter; Self::WORD_SIZE]) -> Self {
+		Self {
+			winning_word,
+
+			current_word: [Letter::A; Self::WORD_SIZE],
+			cursor: 0,
+
+			previous_words: [[(Letter::A, Correctness::Incorrect); Self::WORD_SIZE]; Self::MAX_TRIES],
+			current_try: 0,
+		}
+	}
+
+	pub fn type_letter(&mut self, letter: Letter) {
+		if self.cursor == Self::WORD_SIZE {
+			return;
+		}
+	
+		self.current_word[self.cursor] = letter;
+		self.cursor += 1;
+	}
+	
+	pub fn cancel_letter(&mut self) {
+		if self.cursor != 0 {
+			self.cursor -= 1;
+		}
+	}
+
+	pub fn confirm_word(&mut self) {
+		if self.cursor != Self::WORD_SIZE {
+			return;
+		}
+
+		for i in 0..Self::WORD_SIZE {
+			let mut correctness = Correctness::Incorrect;
+
+			if self.current_word[i] == self.winning_word[i] {
+				correctness = Correctness::Correct;
+			}
+
+			self.previous_words[self.current_try][i] = (self.current_word[i], correctness);
+		}
+
+		for current_letter in self.winning_word {
+			let mut count = 0;
+			for (letter, correctness) in self.previous_words[self.current_try] {
+				if letter == current_letter && correctness == Correctness::Incorrect {
+					count += 1;
+				}
+			}
+
+			for (letter, correctness) in &mut self.previous_words[self.current_try] {
+				if *letter == current_letter {
+					*correctness = Correctness::Misplaced;
+
+					count -= 1;
+					if count == 0 {
+						break;
+					}
+				}
+			}
+		}
+
+		if self.winning_word == self.current_word {
+			todo!("You won!");
+		}
+		
+		self.current_try += 1;
+		if self.current_try == Self::MAX_TRIES {
+			todo!("You lost!");
+		}
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use mlx::*;
+use std::cell::RefCell;
 use std::ffi::CStr;
 
 mod display;
@@ -16,8 +17,9 @@ const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 
 fn main() {
-    let mlx = Mlx::init().expect("Failed to initialize the MiniLibX.");
+    let game = RefCell::new(Game::new([Letter::A, Letter::B, Letter::C, Letter::D, Letter::E]));
 
+    let mlx = Mlx::init().expect("Failed to initialize the MiniLibX.");
     let win = mlx.create_window(WIDTH, HEIGHT, cstr("Test\0")).expect("Failed to initialize the window.");
     let img = mlx.create_image(WIDTH, HEIGHT).unwrap();
     let black_alpha = mlx.create_image_from_xpm_file(cstr("assets/alphabet_black.xpm\0")).unwrap();
@@ -31,11 +33,47 @@ fn main() {
     win.put_image(&green_alpha, 0, 128);
     win.put_image(&grey_alpha, 0, 192);
     mlx.hook_loop(move || {});
+
     win.hook(|KeyPress(keycode)| {
+        let mut game = game.borrow_mut();
+
         if keycode == KeyCode::ESCAPE {
             mlx.stop_loop();
         }
+
+        match keycode {
+            KeyCode::A => game.type_letter(Letter::A),
+            KeyCode::B => game.type_letter(Letter::B),
+            KeyCode::C => game.type_letter(Letter::C),
+            KeyCode::D => game.type_letter(Letter::D),
+            KeyCode::E => game.type_letter(Letter::E),
+            KeyCode::F => game.type_letter(Letter::F),
+            KeyCode::G => game.type_letter(Letter::G),
+            KeyCode::H => game.type_letter(Letter::H),
+            KeyCode::I => game.type_letter(Letter::I),
+            KeyCode::J => game.type_letter(Letter::J),
+            KeyCode::K => game.type_letter(Letter::K),
+            KeyCode::L => game.type_letter(Letter::L),
+            KeyCode::M => game.type_letter(Letter::M),
+            KeyCode::N => game.type_letter(Letter::N),
+            KeyCode::O => game.type_letter(Letter::O),
+            KeyCode::P => game.type_letter(Letter::P),
+            KeyCode::Q => game.type_letter(Letter::Q),
+            KeyCode::R => game.type_letter(Letter::R),
+            KeyCode::S => game.type_letter(Letter::S),
+            KeyCode::T => game.type_letter(Letter::T),
+            KeyCode::U => game.type_letter(Letter::U),
+            KeyCode::V => game.type_letter(Letter::V),
+            KeyCode::W => game.type_letter(Letter::W),
+            KeyCode::X => game.type_letter(Letter::X),
+            KeyCode::Y => game.type_letter(Letter::Y),
+            KeyCode::Z => game.type_letter(Letter::Z),
+            KeyCode::BACKSPACE => game.cancel_letter(),
+            KeyCode::RETURN => game.confirm_word(),
+            _ => (),
+        }
     });
+    
     win.hook(|Destroy| {
         mlx.stop_loop();
     });
