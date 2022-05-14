d8fffd2f878219519edabc879efd48fa6f06ca16
nmathieu <nmathieu@student.42.fr>
2022-05-14 19:09:52 +0200

    set a custom panic message

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use display::*;
 mod game;
 use game::*;
 
+mod custom_panic;
+
 #[inline]
 fn cstr(s: &str) -> &CStr {
     CStr::from_bytes_with_nul(s.as_bytes()).unwrap()
@@ -17,14 +19,16 @@ const WIDTH: u32 = 1280;
 const HEIGHT: u32 = 720;
 
 fn main() {
+    custom_panic::set_custom_panic_hook();
+
     let game = RefCell::new(Game::new([Letter::A, Letter::B, Letter::C, Letter::D, Letter::E]));
 
     let mlx = Mlx::init().expect("Failed to initialize the MiniLibX.");
     let win = mlx.create_window(WIDTH, HEIGHT, cstr("Test\0")).expect("Failed to initialize the window.");
     let img = mlx.create_image(WIDTH, HEIGHT).unwrap();
     let black_alpha = mlx.create_image_from_xpm_file(cstr("assets/alphabet_black.xpm\0")).unwrap();
-    let green_alpha = mlx.create_image_from_xpm_file( cstr("assets/alphabet_green.xpm\0")).unwrap();
-    let yellow_alpha = mlx.create_image_from_xpm_file( cstr("assets/alphabet_yellow.xpm\0")).unwrap();
+    let green_alpha = mlx.create_image_from_xpm_file(cstr("assets/alphabet_green.xpm\0")).unwrap();
+    let yellow_alpha = mlx.create_image_from_xpm_file(cstr("assets/alphabet_yellow.xpm\0")).unwrap();
     let grey_alpha = mlx.create_image_from_xpm_file(cstr("assets/alphabet_grey.xpm\0")).unwrap();
     draw_square(64, 640, 360, 4, &img);
     win.put_image(&img, 0, 0);
@@ -73,7 +77,7 @@ fn main() {
             _ => (),
         }
     });
-    
+
     win.hook(|Destroy| {
         mlx.stop_loop();
     });
