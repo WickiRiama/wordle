2631a6735a565df94ba084c11af5e1bf83b7479b
nmathieu <nmathieu@student.42.fr>
2022-05-15 09:32:45 +0200

    made module-local functions private

--- src/display.rs
+++ src/display.rs
@@ -2,7 +2,7 @@ use mlx::*;
 
 use crate::*;
 
-pub fn set_pixel(dst: &Image, dst_x: u32, dst_y: u32, grayscale: u8) {
+fn set_pixel(dst: &Image, dst_x: u32, dst_y: u32, grayscale: u8) {
     assert!(dst_x < dst.width(), "invalid dst X value ({})", dst_x);
     assert!(dst_y < dst.height(), "invalid dst Y value ({})", dst_y);
 
@@ -17,7 +17,7 @@ pub fn set_pixel(dst: &Image, dst_x: u32, dst_y: u32, grayscale: u8) {
     }
 }
 
-pub fn copy_pixel(dst: &Image, dst_x: u32, dst_y: u32, mut pixel: *const u8) {
+fn copy_pixel(dst: &Image, dst_x: u32, dst_y: u32, mut pixel: *const u8) {
     assert!(dst_x < dst.width(), "invalid dst X value ({})", dst_x);
     assert!(dst_y < dst.height(), "invalid dst Y value ({})", dst_y);
 
@@ -33,7 +33,7 @@ pub fn copy_pixel(dst: &Image, dst_x: u32, dst_y: u32, mut pixel: *const u8) {
     }
 }
 
-pub fn draw_square(size: u32, x: u32, y: u32, weight: u32, img: &Image) {
+fn draw_square(size: u32, x: u32, y: u32, weight: u32, img: &Image) {
     for j in 0..weight {
         for i in 0..=size - 1 - j * 2 {
             set_pixel(img, x + j + i, y + j, 0);
@@ -44,7 +44,7 @@ pub fn draw_square(size: u32, x: u32, y: u32, weight: u32, img: &Image) {
     }
 }
 
-pub fn draw_n_squares(img: &Image, nb_col: u32, nb_row: u32, size: u32) {
+fn draw_n_squares(img: &Image, nb_col: u32, nb_row: u32, size: u32) {
     let mut x = 30;
     let mut y;
     for _ in 0..nb_col {
@@ -57,7 +57,7 @@ pub fn draw_n_squares(img: &Image, nb_col: u32, nb_row: u32, size: u32) {
     }
 }
 
-pub fn init_bg(img: &Image) {
+fn init_bg(img: &Image) {
     for i in 0..img.width() {
         for j in 0..img.height() {
             set_pixel(img, i, j, 255);
@@ -66,7 +66,7 @@ pub fn init_bg(img: &Image) {
     draw_n_squares(img, 5, 6, 64);
 }
 
-pub fn draw_letter(letter: Letter, dst_x: u32, dst_y: u32, dst_img: &Image, alphabet: &Image) {
+fn draw_letter(letter: Letter, dst_x: u32, dst_y: u32, dst_img: &Image, alphabet: &Image) {
     let index = letter as u32;
     let x_alphabet = index * 64;
     let y_alphabet = 0 as u32;
@@ -84,7 +84,7 @@ pub fn draw_letter(letter: Letter, dst_x: u32, dst_y: u32, dst_img: &Image, alph
     }
 }
 
-pub fn draw_current(word: [Letter; 5], row: u32, cursor: usize, img: &Image, alphabet: &Image) {
+fn draw_current(word: [Letter; 5], row: u32, cursor: usize, img: &Image, alphabet: &Image) {
     let mut x = 30;
     let y = 30 + row * 64 + row * 10;
     for i in 0..cursor {
@@ -93,7 +93,7 @@ pub fn draw_current(word: [Letter; 5], row: u32, cursor: usize, img: &Image, alp
     }
 }
 
-pub fn draw_previous(word: [(Letter, Correctness); 5], row: u32, img: &Image, images: &Images) {
+fn draw_previous(word: [(Letter, Correctness); 5], row: u32, img: &Image, images: &Images) {
     let mut x = 30;
     let y = 30 + row * 64 + row * 10;
 	let mut alphabet: &Image;
